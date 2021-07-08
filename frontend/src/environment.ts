34cb28e6a2571ff05f1def469a40f3c4f65d6fa4
Cindy Halim <jcindyhalim@gmail.com>
2021-07-08 00:20:40 -0400

    fix env variablename

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -2,7 +2,7 @@ import * as env from "env-var";
 
 export const config = {
   notionDatabaseId: env
-    .get("REACT_APP_NOTION_DATABSE_ID")
+    .get("REACT_APP_NOTION_DATABASE_ID")
     .required()
     .asString(),
   serviceUrl: env.get("REACT_APP_SERVICE_URL").required().asString(),
