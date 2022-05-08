d2bf0acc50501e1a96e355a054a449b96016bca5
Cindy Halim <jcindyhalim@gmail.com>
2022-05-08 07:22:09 -0400

    refactor: error toast logic and styles

--- frontend/src/components/error-toast.tsx
+++ frontend/src/components/error-toast.tsx
@@ -4,40 +4,52 @@ import { Box, Text } from "rebass";
 
 interface IErrorToastProps {
   isVisible: boolean;
+  setIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
 }
-export const ErrorToast: React.FC<IErrorToastProps> = ({ isVisible }) => {
+export const ErrorToast: React.FC<IErrorToastProps> = ({
+  isVisible,
+  setIsVisible,
+}) => {
   return (
-    <AnimatePresence>
-      {isVisible && (
-        <motion.div
-          layout
-          initial="collapsed"
-          animate="open"
-          exit="collapsed"
-          variants={{
-            open: {
-              opacity: 1,
-              height: "auto",
-              transition: { duration: 0.3 },
-            },
-            collapsed: { opacity: 0, height: 0 },
-          }}
-        >
-          <Box
-            sx={{
-              minHeight: "50px",
-              width: "100%",
-              marginBottom: 30,
-              padding: 10,
-              borderLeft: "5px solid #fc4747",
-              bg: "#ffe3e3",
-              borderRadius: 5,
+    <Box
+      sx={{
+        position: "absolute",
+        top: 20,
+        zIndex: 1,
+        width: ["80%", "80%", "50%"],
+      }}
+    >
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            initial="hide"
+            animate="show"
+            exit="hide"
+            variants={{
+              show: { opacity: 1 },
+              hide: { opacity: 0 },
             }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            onAnimationComplete={() =>
+              setTimeout(() => setIsVisible(false), 3000)
+            }
           >
-            <Text>Something went wrong. Please try again</Text>
-          </Box>
-        </motion.div>
-      )}
-    </AnimatePresence>
+            <Box
+              sx={{
+                minHeight: "50px",
+                width: "100%",
+                marginBottom: 30,
+                padding: 10,
+                borderLeft: "5px solid #fc4747",
+                bg: "#ffe3e3",
+                borderRadius: 5,
+              }}
+            >
+              <Text>Something went wrong. Please try again</Text>
+            </Box>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </Box>
   );
 };
