ac6a4ee542f733e07f6a9265149fe4de48cd0b07
Cindy Halim <jcindyhalim@gmail.com>
2022-05-20 21:34:48 -0400

    feat: home page auth logic

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,17 +1,97 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Flex, Text } from "rebass";
+import { Button } from "../components/button";
 import { useAuth } from "../core/auth/hooks";
 import { getNotionAuthorizationUrl } from "../core/auth/utils";
-import { BaseLayout } from "../layout/base-layout";
+import { theme } from "../layout/theme";
+
+const UnauthenticatedHomeContent = () => {
+  const notionAuthorizeUrl = getNotionAuthorizationUrl();
+
+  return (
+    <>
+      <Text sx={{ ...theme.title, color: theme.colors.black }}>
+        {"notion <> kindle"}
+      </Text>
+      <Text sx={{ ...theme.text, color: theme.colors.black, marginBottom: 40 }}>
+        a collection of tools to improve your e-reading experience
+      </Text>
+      <Button
+        onClick={() => {
+          window.location.href = notionAuthorizeUrl;
+        }}
+      >
+        get started
+      </Button>
+    </>
+  );
+};
+
+interface IFeatures {
+  text: string;
+  route: string;
+}
+
+const AuthenticatedHomeContent = () => {
+  const navigate = useNavigate();
+
+  const features: IFeatures[] = [
+    {
+      text: "> scrape reading list",
+      route: "/prettify",
+    },
+    {
+      text: "> epub to kindle",
+      route: "/epub-to-kindle",
+    },
+    {
+      text: "> upload kindle clippings to notion",
+      route: "/clippings-to-notion",
+    },
+  ];
+
+  return (
+    <>
+      <Text sx={{ ...theme.title, color: theme.colors.black, fontSize: 40 }}>
+        {"notion <> kindle"}
+      </Text>
+      {features.map((feature, idx) => (
+        <Text
+          key={idx}
+          sx={{
+            ...theme.title,
+            color: theme.colors.black,
+            fontSize: 20,
+            cursor: "pointer",
+          }}
+          onClick={() => navigate(feature.route)}
+        >
+          {feature.text}
+        </Text>
+      ))}
+    </>
+  );
+};
 
 export const Home = () => {
   const { isAuthenticated } = useAuth();
-  const notionAuthorizeUrl = getNotionAuthorizationUrl();
 
-  return !isAuthenticated ? (
-    <BaseLayout title="notion <> kindle" buttons={[]}>
-      <a href={notionAuthorizeUrl}>connect notion</a>
-    </BaseLayout>
-  ) : (
-    <>AUTHENTICATED!</>
+  return (
+    <Flex
+      sx={{
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      {!isAuthenticated ? (
+        <UnauthenticatedHomeContent />
+      ) : (
+        <AuthenticatedHomeContent />
+      )}
+    </Flex>
   );
 };
