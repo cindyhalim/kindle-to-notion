afdfb0b0e6ae0fbde42be24324f09ba97dd6f85e
Cindy Halim <jcindyhalim@gmail.com>
2022-05-21 02:58:29 -0400

    fix: remove notion database id from env config

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -2,9 +2,5 @@ import * as env from "env-var";
 
 export const config = {
   notionClientId: env.get("REACT_APP_NOTION_CLIENT_ID").required().asString(),
-  notionDatabaseId: env
-    .get("REACT_APP_NOTION_DATABASE_ID")
-    .required()
-    .asString(),
   serviceUrl: env.get("REACT_APP_SERVICE_URL").required().asString(),
 };
