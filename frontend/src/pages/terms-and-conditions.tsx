b9a7eadee09096ae1673e99702ea7b7040c8ac49
Cindy Halim <jcindyhalim@gmail.com>
2022-05-27 19:00:04 -0400

    feat: copy changes

--- frontend/src/pages/terms-and-conditions.tsx
+++ frontend/src/pages/terms-and-conditions.tsx
@@ -5,18 +5,19 @@ import { BaseLayout } from "../layout/base-layout";
 export const TermsAndConditions = () => (
   <BaseLayout title={"terms and conditions"} buttons={[]}>
     <Text sx={{ fontWeight: 300 }}>
-      we do not sell or monetize your personal data or content in any way.
+      {"notion <> kindle"} does not sell or monetize your personal data or
+      content in any way.
       <br />
       <br />
-      we are restricted to accessing the notion page you selected upon getting
-      started.
+      we are restricted to accessing the notion page you agree to select upon
+      getting started.
       <br />
       <br />
-      we utilize google search to look for epub links and use the first link
-      obtained from the result, which may come from an unknown source that could
-      contain malicious content. please be advised when downloading content from
-      unknown sources. we are not liable for issues that can arise from doing
-      so.
+      {"notion <> kindle"} utilizes google search to look for epub links and use
+      the first link obtained from the result, which may come from an unknown
+      source that could contain malicious content. please be advised when
+      downloading content from unknown sources. {"notion <> kindle"} are not
+      liable for issues that can arise from doing so.
     </Text>
   </BaseLayout>
 );
